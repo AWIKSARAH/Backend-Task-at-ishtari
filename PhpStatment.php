b5ec5af05a768ebf0ae0bbf02e589865ab614e07
Sarah Awik <awiksarah@gmail.com>
2023-10-19 23:46:35 +0300

    Php  Loop

--- PhpStatment.php
+++ PhpStatment.php
@@ -1,10 +1,13 @@
 <?php
 
+// This code checks if the sum of $x and $y is greater than or equal to 20.
+// If it is, the code prints "Greater or equal 20". Otherwise, the code prints "Smaller 20".
+
 $x = 10;
 $y = 10;
 
 if ($x + $y >= 20) {
-    echo "Greater or equal  20";
+    echo "Greater or equal 20";
 } else {
     echo "Smaller 20";
 }
