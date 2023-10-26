140422c9cc39b4aafb2740f4efb944d04294c31a
Sarah Awik <awiksarah@gmail.com>
2023-10-26 19:57:33 +0300

    add the db table

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 ### BACKEND DEVELOPER AT ISHTARI
+
+# Learn PHP and MySQL
+
+## Branches
+
+- **basic-php:** Lessons and exercises on the basics of PHP, including variables, data types, string functions, operators, conditional statements, loops, functions, arrays, and global variables.
+- **MySQL:** Lessons and exercises on more advanced PHP topics, such as date and time, file upload, and JSON.
+
+## MySQL
+
+MySQL is a relational database management system (RDBMS) that is used to store and manage data.
+
+## API
+
+The API is hosted at the following URL:
+
+## Installation
+
+1. Clone this repository.
+
+
+## Import the database
+
+To import the database, you can use the following command:
+
+## Fetch to get the branch
+
+To fetch to get the branch, you can use the following code:
---
+++ ishtari_task.sql
@@ -0,0 +1,111 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 26, 2023 at 06:55 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ishtari_task`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `id` int(11) NOT NULL,
+  `category_name` varchar(50) NOT NULL,
+  `status` tinyint(1) NOT NULL,
+  `image` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`id`, `category_name`, `status`, `image`) VALUES
+(3, 'New Category 2', 0, 'category_image.jpg'),
+(4, 'New Category 6', 0, 'category_image.jpg'),
+(5, 'h6', 1, 'category_image.jpg'),
+(6, 'h6', 1, 'category_image.jpg'),
+(7, 'h6', 1, 'category_image.jpg'),
+(8, 'h6', 1, 'category_image.jpg'),
+(9, 'h6', 1, 'category_image.jpg'),
+(10, 'h6', 1, 'category_image.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE `product` (
+  `id` int(11) NOT NULL,
+  `product_name` varchar(50) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `price` int(11) NOT NULL,
+  `category` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `category` (`category`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `product`
+--
+ALTER TABLE `product`
+  ADD CONSTRAINT `product_ibfk_1` FOREIGN KEY (`category`) REFERENCES `category` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
